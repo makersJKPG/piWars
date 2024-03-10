478ac2f686f3d6bd5d45a9df100d713580e05fc5
Arvind Shah <arvind.shah@husqvarnagroup.com>
2024-03-10 11:39:46 +0100

    created lavapalava project, added uart test script

---
+++ lavapalava/lavapalava.py
@@ -0,0 +1,42 @@
+
+
+
+
+
+# todo:
+# - straight line driving
+#   + synchronized acceleration on both wheels
+#   + maintain constant speed on both wheels
+#   + synchronize distance travelled on both wheels
+#   + calculate pose from wheel ticks
+#   + bonus: calculate path plan to get from current pose to desired pose
+#   + bonus: execute planned path
+# - line following
+#   + in straight line
+#   + through left/right bend
+#   + over hump
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
