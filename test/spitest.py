9cbf275acde1ae4f9ad7a29da928fbc81e1bb11e
Arvind Shah <arvind.shah@esscotti.com>
2024-03-18 22:29:20 +0100

    tidied up code after change in spi protocol

--- test/spitest.py
+++ test/spitest.py
@@ -5,19 +5,18 @@ bus = 0
 device = 0
 spi = spidev.SpiDev()
 spi.open(bus, device)
-
 spi.bits_per_word = 8
 spi.max_speed_hz = 10000
 spi.mode = 1
+spi.read0 = False
+spi.cshigh = False
 
-msg = [ 0xba, 0, 0, 0, 0 ]
+
+#msg = [ 0xba, 0, 0, 0, 0]
+msg = [ 20 ] 
 print("Sending: {}".format(msg))
 cs = spi.xfer2(msg)
-
-b = spi.readbytes(5)
-print("Received: {}".format(b))
-
-#time.sleep(5)
+print("Received: {}".format(cs))
 
 spi.close()
 
